ccda65cb6bf98e4911c325eed54b9cff9765d286
Andrew Mokotoff <amokotoff@gmail.com>
2015-08-11 22:09:55 -0400

    Started timer program...

--- Timer.c
+++ Timer.c
@@ -4,13 +4,15 @@ SimpleTimer timer;
 
 void turn_off_pump()
 {
-	//turn off pump here
+	// turn off pump
+	print("Pump off!\n");
 	toggle(pump_timer);
 }
 
 void turn_on_pump()
 {
 	// turn on pump here
+	print("Pump on!\n");
 	timer.setTimeout(60000, turn_off_pump();
 	toggle(pump_timer);
 }
@@ -18,12 +20,14 @@ void turn_on_pump()
 void off_light()
 {
 	// off light
+	print("Light off!\n");
 	toggle(light_timer);
 }
 
 void on_light()
 {
 	// turn on light
+	print("Light on!\n");
 	timer.setTimeout(432000000, off_light);
 	toggle(light_timer);
 }
