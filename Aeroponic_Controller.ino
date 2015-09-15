06acad8110335ec014afc8c8da73fcd006d3c605
Andrew Mokotoff <amokotoff@gmail.com>
2015-09-15 15:25:02 -0400

    Added digital writes

--- Aeroponic_Controller.ino
+++ Aeroponic_Controller.ino
@@ -2,13 +2,15 @@
 
 SimpleTimer timer;
 
-int pump_timer;
-int light_timer;
+int pump_timer, light_timer;
+const int pump_pin = 12;
+const int light_pin = 13;
 
 void turn_off_pump()
 {
   // turn off pump
   Serial.println("Pump off!");
+  digitalWrite(pump_pin, LOW);
   timer.toggle(pump_timer);
 }
 
@@ -16,6 +18,7 @@ void turn_on_pump()
 {
   // turn on pump here
   Serial.println("Pump on!");
+  digitalWrite(pump_pin, HIGH);
   timer.setTimeout(15000, turn_off_pump);
   timer.toggle(pump_timer);
 }
@@ -24,6 +27,7 @@ void off_light()
 {
   // off light
   Serial.println("Light off!");
+  digitalWrite(light_pin, LOW);
   timer.toggle(light_timer);
 }
 
@@ -31,6 +35,7 @@ void on_light()
 {
   // turn on light
   Serial.println("Light on!");
+  digitalWrite(light_pin, HIGH);
   timer.setTimeout(20000, off_light);
   timer.toggle(light_timer);
 }
@@ -38,6 +43,8 @@ void on_light()
 void setup()
 {
   Serial.begin(9600);
+  pinMode(pump_pin, OUTPUT);
+  pinMode(light_pin, OUTPUT);
   Serial.println("Starting!");
   pump_timer = timer.setInterval(10000, turn_on_pump);
   light_timer = timer.setInterval(20000, on_light);
@@ -45,6 +52,8 @@ void setup()
 
 void loop()
 {
+  digitalWrite(pump_pin, LOW);
+  digitalWrite(light_pin, LOW);
   timer.run();
 }
 
